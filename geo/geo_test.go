b3674f49a9bbed3eded1b18f374e82187f5cf235
AlariCode <antonlarichev@gmail.com>
2024-06-07 10:51:19 +0300

    Arrange Act Assert

---
+++ geo/geo_test.go
@@ -0,0 +1,12 @@
+package geo_test
+
+import "testing"
+
+func TestGetMyLocation(t *testing.T) {
+	// Arange - подготовка, expected результат, данные для функции
+
+	// Act - выполняем функцию
+
+	// Assert - проверка результата с expected
+
+}
